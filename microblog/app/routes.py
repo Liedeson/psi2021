e111a6527430b6f3058a6957679857fcbcc17771
Liedeson <liedeson.nascimento@escolar.ifrn.edu.br>
2021-08-19 14:37:03 -0300

    Alteraçôes

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,8 +1,16 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Ola Mundo!"
+    usuario = {"nome": "Liedeson", "sobrenome": "Moraes"}
+    
+    return render_template('index.html', usuario=usuario, titulo='Pagina inicial')
+    
+@app.route('/contato')
+def contato():
+    return render_template('contato.html', titulo="Contato")
+
 
 
